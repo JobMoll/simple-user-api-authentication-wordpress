a8be3a212051e75fd123515f83f51260bb26cb7d
Job Moll <jobmoll@gmail.com>
2020-09-23 15:31:40 +0200

    Clearer labels

--- includes/plugin-classes/class-add-fields-to-user-profile.php
+++ includes/plugin-classes/class-add-fields-to-user-profile.php
@@ -11,10 +11,10 @@ function suaa_show_extra_profile_fields($currentUser) {
     <table class="form-table">
 
 		<tr>
-			<th><label for="suaa_refresh_token_valid_length_user">Refresh token length in hours (user specific)</label></th>
+			<th><label for="suaa_refresh_token_valid_length_user">Refresh token valid length</label></th>
 			<td>
-				<input type="text" name="suaa_refresh_token_valid_length_user" id="suaa_refresh_token_valid_length_user" class="regular-text" value="<?php echo esc_attr(get_the_author_meta('suaa_refresh_token_valid_length_user', $currentUser->ID)); ?>"><br/>
-				  <span class="description">Don't let the user get a new access token when this expires.</span>
+				<input disabled type="text" name="suaa_refresh_token_valid_length_user" id="suaa_refresh_token_valid_length_user" class="regular-text" value="<?php echo esc_attr(get_the_author_meta('suaa_refresh_token_valid_length_user', $currentUser->ID)); ?>"><br/>
+				  <span class="description">This has been set by the user. It doesn't let the user get a new access token when this expires.</span>
 			</td>
 		</tr>
 		
@@ -22,7 +22,7 @@ function suaa_show_extra_profile_fields($currentUser) {
 			<th><label for="suaa_latest_refresh_token">Latest refresh token</label></th>
 			<td>
 				<input disabled type="text" name="suaa_latest_refresh_token" id="suaa_latest_refresh_token" class="regular-text" value="<?php echo esc_attr(get_the_author_meta('suaa_latest_refresh_token', $currentUser->ID)); ?>"><br/>
-				  <span class="description">The latest refresh token of this user.</span>
+				  <span class="description">The latest refresh token of this user. (for debugging)</span>
 			</td>
 		</tr>
 
@@ -30,7 +30,7 @@ function suaa_show_extra_profile_fields($currentUser) {
 			<th><label for="suaa_latest_access_token">Latest access token</label></th>
 			<td>
 				<input disabled type="text" name="suaa_latest_access_token" id="suaa_latest_access_token" class="regular-text" value="<?php echo esc_attr(get_the_author_meta('suaa_latest_access_token', $currentUser->ID)); ?>"><br/>
-				  <span class="description">The latest access token of this user.</span>
+				  <span class="description">The latest access token of this user. (for debugging)</span>
 			</td>
 		</tr>		
 
@@ -43,10 +43,10 @@ function suaa_show_extra_profile_fields($currentUser) {
 		</tr>
 		
 		<tr>
-			<th><label for="suaa_wrong_brute_force_attempts_end_time">Brute force wrong attempts end time</label></th>
+			<th><label for="suaa_wrong_brute_force_attempts_end_time">Brute force protection end time</label></th>
 			<td>
 				<input disabled type="text" name="suaa_wrong_brute_force_attempts_end_time" id="suaa_wrong_brute_force_attempts_end_time" class="regular-text" value="<?php echo gmdate("Y-m-d H:i:s", intval(esc_attr(get_the_author_meta('suaa_wrong_brute_force_attempts_end_time', $currentUser->ID)))); ?>"><br/>
-				  <span class="description">The till the user can try to login in again.</span>
+				  <span class="description">The user can try to log in again at this time.</span>
 			</td>
 		</tr>
 		
